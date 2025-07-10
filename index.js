5b4cac88324c66cb5b5c1db709ef74851ca3120e
mac <mac@macs-MacBook-Air.local>
2025-07-10 14:30:15 +0530

    Fix: remove CONVERT_TZ to avoid double offset

--- index.js
+++ index.js
@@ -155,7 +155,7 @@ failed` });
 
     const fetchQuery = `
       SELECT users.name AS username, travel_plans.destination,
-             travel_plans AS time
+             travel_plans.time AS time
       FROM travel_plans
       INNER JOIN users ON travel_plans.user_id = users.id
       ORDER BY travel_plans.time DESC
