11d6dc53f2c3815f7dc75d5b356443e7cff74c33
mac <mac@macs-MacBook-Air.local>
2025-07-26 16:17:10 +0530

    Fix:Root

--- index.js
+++ index.js
@@ -173,7 +173,7 @@ app.get('/', (req, res) => {
 
 // 🌐 Start server
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`🚀 Server running on ${PORT}`);
 });
 
