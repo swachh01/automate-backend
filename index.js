a08f1e98621cd89a9e2271da7a66b405353f0c5c
mac <mac@macs-MacBook-Air.local>
2025-08-29 19:29:29 +0530

    reloaded

--- index.js
+++ index.js
@@ -525,5 +525,6 @@ error` });
 
 // ===================== START =====================
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`🚀 Server listening on ${PORT}`));
+app.listen(PORT,"0.0.0.0", () => console.log(`🚀 Server listening on 
+${PORT}`));
 
