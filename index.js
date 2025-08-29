fc776f1ffca16a00a9f102d290abe07b9d62e868
mac <mac@macs-MacBook-Air.local>
2025-08-29 18:45:22 +0530

    Reloaded

--- index.js
+++ index.js
@@ -251,6 +251,6 @@ false, message: 'Missing senderId or receiverId' });
 app.get("/health", (req, res) => res.status(200).send("OK"));
 
 // ===================== START SERVER =====================
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 
