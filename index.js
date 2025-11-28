6bf2354b55f5299e8ec8d2550c908c086fca1ecf
mac <mac@macs-MacBook-Air.local>
2025-11-28 23:28:17 +0530

    Improved notification

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-krequire("dotenv").config();
+require("dotenv").config();
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 const bcrypt = require('bcryptjs'); 
