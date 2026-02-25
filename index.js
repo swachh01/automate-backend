d17710af29f20dc7da339faeae5d3b34d9b92e60
mac <mac@macs-MacBook-Air.local>
2026-02-26 04:39:02 +0530

    Added security 5

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ const client = new twilio(accountSid, authToken);
 
 const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5, // Limit each IP to 5 requests per 15 minutes
+    max: 15, // Limit each IP to 5 requests per 15 minutes
     message: { 
         success: false, 
         message: "Too many attempts from this IP, please try again after 15 minutes" 
